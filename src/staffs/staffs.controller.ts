d19a93be785e464dd4cb7766bc4785ebbf028f96
NorbertLoh <lohnorbert@gmail.com>
2024-02-21 18:02:21 +0800

    types

--- src/staffs/staffs.controller.ts
+++ src/staffs/staffs.controller.ts
@@ -4,7 +4,7 @@ import { StaffsService } from './staffs.service';
 import { ApiConsumes, ApiBody } from '@nestjs/swagger';
 import { FileInterceptor } from '@nestjs/platform-express';
 
-import 'multer';
+import { Express } from 'express'
 
 @Controller('staffs')
 export class StaffsController {
