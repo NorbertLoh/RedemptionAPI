4a5de96a276ff94193a409876e087b967eabf0ee
NorbertLoh <lohnorbert@gmail.com>
2024-02-21 17:10:46 +0800

    types

--- src/staffs/staffs.service.ts
+++ src/staffs/staffs.service.ts
@@ -7,6 +7,7 @@ import { readFileSync } from 'fs'
 import { Staff } from './entities/staff.entity'
 import { parse } from 'csv-parse';
 
+import { Express } from "express";
 
 @Injectable()
 export class StaffsService {
